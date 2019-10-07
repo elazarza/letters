aef7a5c2461a89f1d419d5a92949e54c998b5734
elazarza <44997619+elazarza@users.noreply.github.com>
2019-10-07 15:12:48 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild, ElementRef  } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+
 })
-export class AppComponent {
-  title = 'letters';
+export class AppComponent   {
+  colors = [
+    {value: 'black', viewValue: 'שחור'},
+    {value: 'grey', viewValue: 'אפור'},
+    {value: 'blue', viewValue: 'כחול'}
+  ];
+  @ViewChild('top', {static: true}) top: ElementRef;
+  @HostListener('window:scroll', ['$event']) scroll(e) {
+
+    // this.top.nativeElement.scrollIntoView( { behavior: 'smooth', block: 'start' });
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatButtonModule, MatToolbarModule } from '@angular/material';
+import { CommonModule } from '@angular/common';
+import {CrystalLightboxModule} from '@crystalui/angular-lightbox';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSelectModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    CommonModule,
+    CrystalLightboxModule
+  ],
+  declarations: [
+    AppComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
